247055e10c1b96ab89bb5c988247879282836d68
MikiBen <mikinarloch@gmail.com>
2023-01-19 09:55:51 +0100

    Create Json Model and readFile

---
+++ SportRadarExercise/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import file.JsonFile;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        JsonFile jsonFile = new JsonFile();
+
+        jsonFile.readFile();
+        jsonFile.showMostProbableResult(5);
+
+    }
+}
